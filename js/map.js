d925c18dec533813281f68da272c951bbd19ef6d
chrisamico <eyeseast@gmail.com>
2013-06-01 01:37:35 -0400

    Trying out updates in SVG. Probably not realistic.

--- js/map.js
+++ js/map.js
@@ -7,7 +7,10 @@ var margin = {top: 10, right: 10, bottom: 10, left: 10}
   , width = parseInt(d3.select('#map').style('width'))
   , width = width - margin.right - margin.left
   , height = parseInt(d3.select('#map').style('height'))
-  , height = height - margin.top - margin.bottom;
+  , height = height - margin.top - margin.bottom,
+  data;
+
+var dateFormat = d3.time.format('usdm%y%m%d');
 
 var map = d3.select('#map').append('svg')
     .style('width', width)
@@ -25,14 +28,17 @@ queue()
 
 function render(err, us, drought) {
 
-    window.data = {
+    data = {
         drought: drought,
-        us: us
+        us: us,
+        weeks: {}
     };
 
-    var drought = topojson.feature(drought, drought.objects['usdm130101'])
+    var weeks = d3.keys(drought.objects)
+      , dm = topojson.feature(drought, drought.objects[weeks[weeks.length - 1]])
       , land = topojson.mesh(us, us.objects.land)
-      , states = topojson.feature(us, us.objects.states);
+      , states = data.states =  topojson.feature(us, us.objects.states)
+      , week;
 
     map.append('path')
         .attr('class', 'land')
@@ -40,7 +46,7 @@ function render(err, us, drought) {
         .attr('d', path);
 
     map.selectAll('path.drought')
-        .data(drought.features)
+        .data(dm.features)
       .enter().append('path')
         .attr('d', path)
         .attr('class', function(d) { return "drought DM-" + d.id; });
@@ -50,4 +56,20 @@ function render(err, us, drought) {
       .enter().append('path')
         .attr('d', path)
         .attr('class', 'states');
+
+    for (var i = weeks.length - 1; i >= 0; i--) {
+        week = weeks[i];
+        data.weeks[week] = topojson.feature(drought, drought.objects[week])
+    };
+
 };
+
+function update(week) {
+    var dm = data.weeks[week];
+
+    map.selectAll('path.drought')
+        .data(dm.features)
+        .attr('d', path);
+
+    map.selectAll('path.states').attr('d', path);
+}
